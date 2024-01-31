ab3af2c7369cd5c584634b08476114cb2717878b
沈林强 <12514942+shen-linqiang@user.noreply.gitee.com>
2024-01-31 12:56:56 +0800

    feat: 集成antd

--- src/index.js
+++ src/index.js
@@ -4,15 +4,29 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // import store from "./store/index";
-import store from "./store/toolkitIndex"; 
-import { Provider } from "react-redux";
+import store from "./store/toolkitIndex"; // 全局状态仓库
+import { Provider } from "react-redux"; // 全局状态传递至所有组件
+import { ConfigProvider } from "antd"; // 定制主题
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token，影响范围大
+          colorPrimary: "#00b96b",
+          borderRadius: 2,
+
+          // 派生变量，影响范围小
+          colorBgContainer: "#f6ffed",
+        },
+      }}
+    >
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ConfigProvider>
   </React.StrictMode>
 );
 
--- src/views/menu/son1/index.js
+++ src/views/menu/son1/index.js
@@ -1,5 +1,13 @@
+import { Button } from "antd";
+
 function son1() {
-  return <div className="son2-page">son2-page</div>;
+  return (
+    <div className="son2-page">
+      son2-page
+      <Button type="primary" >primary按钮</Button>
+      <Button type="default" >default按钮</Button>
+    </div>
+  );
 }
 
 export default son1;
